25c0f96d551c315bd8178c614fe1341ea997b254
Mary Strodl <ipadlover8322@gmail.com>
2022-10-24 13:53:31 -0400

    fmt/clippy pass

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-extern crate reqwest;
-extern crate gatekeeper_members;
-extern crate libgatekeeper_sys;
-extern crate chrono;
-
-use gatekeeper_members::{GateKeeperMemberListener, FetchError};
+use chrono::prelude::*;
+use gatekeeper_members::{FetchError, GateKeeperMemberListener};
 use libgatekeeper_sys::Nfc;
-use std::process::{Command, ExitStatus};
+use rand::prelude::SliceRandom;
 use reqwest::StatusCode;
 use serde_json::json;
 use std::env;
-use chrono::prelude::*;
-use rand::prelude::SliceRandom;
+use std::process::{Command, ExitStatus};
 
 fn get_volume() -> &'static str {
     let now = Local::now();
@@ -18,29 +13,32 @@ fn get_volume() -> &'static str {
     let weekday = now.weekday();
 
     if weekday == Weekday::Sun {
-        if (hour < 1 || hour >= 7) && 23 >= hour {
+        if !(1..7).contains(&hour) && 23 >= hour {
             return "100";
         }
     } else if weekday == Weekday::Sat {
-        if hour < 1 || hour >= 7 {
+        if !(1..7).contains(&hour) {
             return "100";
         }
-    } else {
-        if 23 >= hour && hour >= 7 {
-            return "100";
-        }
+    } else if (7..=23).contains(&hour) {
+        return "100";
     }
 
-    return "73";
+    "73"
 }
 
 fn play_music(path: &str, do_cap: bool) -> Result<(), ExitStatus> {
     let mut cmd = &mut Command::new("ffplay");
     cmd = cmd
-        .arg(path.clone()).arg("-b:a").arg("64k")
-        .arg("-nodisp").arg("-autoexit")
-        .arg("-volume").arg(get_volume())
-        .arg("-loglevel").arg("error");
+        .arg(path)
+        .arg("-b:a")
+        .arg("64k")
+        .arg("-nodisp")
+        .arg("-autoexit")
+        .arg("-volume")
+        .arg(get_volume())
+        .arg("-loglevel")
+        .arg("error");
     if do_cap {
         cmd = cmd.arg("-t").arg("30");
     }
@@ -56,10 +54,10 @@ fn play_music(path: &str, do_cap: bool) -> Result<(), ExitStatus> {
                 } else {
                     return Err(status);
                 }
-            },
+            }
             Ok(None) => {
                 // TODO: Dammit here
-            },
+            }
             Err(e) => println!("Error waiting?? {:?}", e),
         }
     }
@@ -67,9 +65,7 @@ fn play_music(path: &str, do_cap: bool) -> Result<(), ExitStatus> {
 
 fn scan_complete(uid: &str) -> String {
     match uid {
-        "mom" => {
-            return "special/aaa.mp3".to_string();
-        },
+        "mom" => "special/aaa.mp3".to_string(),
         _ => {
             // get all files from scans folder
             let files = std::fs::read_dir("scans").unwrap();
@@ -82,24 +78,27 @@ fn scan_complete(uid: &str) -> String {
     }
 }
 
-fn get_audiophiler(http: reqwest::blocking::Client, harold_auth: &str, uid: String) -> Result<String, RequestError> {
-    let response = http.post(
-        "https://audiophiler.csh.rit.edu/get_harold/".to_string() + &uid
-    ).json(&json!({
-        "auth_key": harold_auth
-    })).send();
+fn get_audiophiler(
+    http: reqwest::blocking::Client,
+    harold_auth: &str,
+    uid: String,
+) -> Result<String, RequestError> {
+    let response = http
+        .post("https://audiophiler.csh.rit.edu/get_harold/".to_string() + &uid)
+        .json(&json!({ "auth_key": harold_auth }))
+        .send();
     match response {
         Ok(res) => match res.status() {
             StatusCode::OK => Ok(res.text().unwrap()),
             status => {
                 println!("Audiophiler responded non-200: {:?}", status);
                 Err(RequestError::StatusCode(status))
-            },
+            }
         },
         Err(err) => {
             println!("Couldn't fetch harold for user: {:?}", err);
             Err(RequestError::Unknown)
-        },
+        }
     }
 }
 
@@ -113,13 +112,17 @@ enum RunFailure {
     RequestError(RequestError),
 }
 
-fn run_harold(http: reqwest::blocking::Client, harold_auth: String, uid: String) -> Result<(), RunFailure> {
+fn run_harold(
+    http: reqwest::blocking::Client,
+    harold_auth: String,
+    uid: String,
+) -> Result<(), RunFailure> {
     // Hopefully we don't crash? lol
-    if let Err(err) = play_music(&scan_complete(&uid.clone()), false) {
+    if let Err(err) = play_music(&scan_complete(&uid), false) {
         return Err(RunFailure::ExitCode(err));
     }
     println!("Played scan complete");
-    match get_audiophiler(http, &harold_auth, uid.clone()) {
+    match get_audiophiler(http, &harold_auth, uid) {
         Ok(sound) => match play_music(&sound, true) {
             Ok(_) => Ok(()),
             Err(err) => Err(RunFailure::ExitCode(err)),
@@ -132,13 +135,12 @@ fn main() {
     let mut nfc = Nfc::new().unwrap();
     let mut listener = GateKeeperMemberListener::new(
         &mut nfc,
-        env::var("HAROLD_GK_READER").unwrap_or(
-            "pn532_uart:/dev/ttyUSB0".to_string()
-        ).to_string()
-    ).unwrap();
+        env::var("HAROLD_GK_READER").unwrap_or_else(|_| "pn532_uart:/dev/ttyUSB0".to_string()),
+    )
+    .unwrap();
     let http = reqwest::blocking::Client::new();
 
-    let harold_auth = env::var("HAROLD_AUTH").unwrap().to_string();
+    let harold_auth = env::var("HAROLD_AUTH").unwrap();
 
     loop {
         if let Some(association) = listener.wait_for_user() {
@@ -146,20 +148,26 @@ fn main() {
             println!("Read {}", association);
             match listener.fetch_user(association) {
                 Ok(value) => {
-                    println!("Got user with name {}", value["user"]["uid"].as_str().unwrap());
+                    println!(
+                        "Got user with name {}",
+                        value["user"]["uid"].as_str().unwrap()
+                    );
                     match run_harold(
                         http.clone(),
                         harold_auth.clone(),
-                        value["user"]["uid"].as_str().unwrap().to_string()
+                        value["user"]["uid"].as_str().unwrap().to_string(),
                     ) {
                         Ok(_) => println!("Played harold for user!"),
-                        Err(RunFailure::ExitCode(status)) =>
-                            eprintln!("Failed to play! {}", status),
-                        Err(RunFailure::RequestError(
-                            RequestError::StatusCode(response_code))) =>
-                            eprintln!("Couldn't get user's harold! Server responded with {}", response_code),
-                        Err(RunFailure::RequestError(
-                            RequestError::Unknown)) => {
+                        Err(RunFailure::ExitCode(status)) => {
+                            eprintln!("Failed to play! {}", status)
+                        }
+                        Err(RunFailure::RequestError(RequestError::StatusCode(response_code))) => {
+                            eprintln!(
+                                "Couldn't get user's harold! Server responded with {}",
+                                response_code
+                            )
+                        }
+                        Err(RunFailure::RequestError(RequestError::Unknown)) => {
                             eprintln!("Couldn't get user's harold, not sure why!");
                         }
                     }
